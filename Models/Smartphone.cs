481a3cacd86041e87476324b4599a548b9b24261
delcio-torres <delcio.at@gmail.com>
2022-10-10 14:56:57 -0300

    Completando as classes Iphone e Nokia - implementando o programa pricinpal

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,31 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
-        {
+        public Smartphone(){
+
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria){
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Telefone {Modelo} ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Telefone {Modelo} recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
+Console.Clear();
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Smartphone nokia = new Nokia("98856-5689", "MK 358", "5565989898989", 50 );
+Smartphone iphone = new Iphone("99138-3002", "Iphone 10", "5565989898981", 50 );
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Watsapp");
+Console.WriteLine();    
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Sinuca");
+Console.WriteLine();
+
